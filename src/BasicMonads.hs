315f5aadaf17e3131a9f7b56d456083931f6c157
Andrei Anton <io@neuronq.ro>
2015-08-22 04:33:43 -0700

    simplify BasicMonads

--- src/BasicMonads.hs
+++ src/BasicMonads.hs
@@ -1,39 +1,50 @@
-import Data.Maybe
-
-
-maybeRead :: Read a => String -> Maybe a
-maybeRead s = case reads s of
-                  [(x,"")]    -> Just x
-                  _           -> Nothing
-
-
-getListFromString :: String -> Maybe [Integer]
-getListFromString str = maybeRead $ "[" ++ str ++ "]"
+module BasicMonads (basicMonadsMain1, basicMonadsMain2, basicMonadsMain3) where
 
 
 askUser :: IO [Integer]
 askUser = do
-  putStrLn "Enter a list of numbers (separated by comma):"
-  input <- getLine
-  let maybeList = getListFromString input in
-      case maybeList of
-          Just l  -> return l
-          Nothing -> askUser
+    putStrLn "Enter a list of numbers (separated by comma):"
+    input <- getLine
+    case reads $ "[" ++ input ++ "]" of
+        [(list, "")] -> return list
+        _            -> askUser
 
 
+-- "hand made" `bind`
 bind :: IO a -> (a -> IO b) -> IO b
 bind action1 action2 = do
     x <- action1
     action2 x
 
 
+-- "hand made" `blindBind`
 blindBind :: IO a -> IO b -> IO b
 blindBind action1 action2 =
     bind action1 (\_ -> action2)
 
 
-main :: IO ()
-main =
-  (askUser `bind` (\list ->
-  (print . (2+) . sum) list)) `blindBind`
-  (putStrLn "That was fun...")
+-- using "do" notation
+basicMonadsMain1 :: IO ()
+basicMonadsMain1 = do
+    putStrLn "In basicMonadsMain1..."
+    list <- askUser
+    print $ (sum list) + 1
+    putStrLn "That was fun..."
+
+
+-- using Haskell's ">>=" ("bind" or "shove") and ">>" ("blind bind")
+basicMonadsMain2 :: IO ()
+basicMonadsMain2 =
+    putStrLn "In basicMonadsMain2..." >>
+    askUser >>= (\list ->
+    print $ (sum list) + 2) >>
+    putStrLn "That was fun..."
+
+
+-- using "hand made" `bind` and `blindBind`
+basicMonadsMain3 :: IO ()
+basicMonadsMain3 =
+    putStrLn "In basicMonadsMain3..." >>
+    askUser `bind` (\list ->
+    print $ (sum list) + 3) `blindBind`
+    (putStrLn "That was fun...")
---
+++ src/Main.hs
@@ -0,0 +1,9 @@
+import BasicMonads
+
+
+main = do
+    putStrLn "Hello from the main module!"
+    basicMonadsMain1
+    basicMonadsMain2
+    basicMonadsMain3
+    
